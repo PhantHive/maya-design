cb93e8450e791d793238168d946fe3a2b9edabfb
PhantHive <zakaria.chaouki@ipsa.fr>
2024-03-05 13:50:20 +0100

    feat(responsive): phone calc for height bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import './components/Body';
 import { Body } from './components/Body.tsx';
 import { Header } from './components/Header.tsx';
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+        const setVh = () => {
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        };
+
+        window.addEventListener('resize', setVh);
+        setVh();
+
+        return () => {
+            window.removeEventListener('resize', setVh);
+        };
+    }, []);
+
     return (
         <>
             <div className="App">
