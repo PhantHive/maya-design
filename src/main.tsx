e90bbce6ecd37b8c20caa9eed85291027d3cd719
PhantHive <zakaria.chaouki@ipsa.fr>
2024-07-02 10:56:03 +0200

    fix: responsive

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './styles/index.css';
+import './styles/index.scss';
 import './styles/body.scss';
 import './styles/header.scss';
 import './styles/footer.scss';
